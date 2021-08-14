8a10b58bac39ca7c778736eab73bae17218f4b75
Tyler Koenig <tylerkng96@icloud.com>
2021-08-14 14:50:33 -0400

    image-upload-complete

---
+++ src/main/java/wecancatchit/werehooked/Storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package wecancatchit.werehooked.Storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+	public StorageFileNotFoundException(String message) {
+		super(message);
+	}
+
+	public StorageFileNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
